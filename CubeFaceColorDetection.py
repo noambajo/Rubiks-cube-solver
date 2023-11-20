59b5a70bc1e610bc794bebf15c9212ab3a95b896
Your Name <familygame2006@gmail.com>
2023-11-20 03:08:31 +0200

    still trying

--- CubeFaceColorDetection.py
+++ CubeFaceColorDetection.py
@@ -1,12 +1,9 @@
 import cv2
-cap = cv2.VideoCapture(0)   # opens the main camera (built-in camera for laptops)
+cap = cv2.VideoCapture(0)
 while True:
-    success, img = cap.read()   # Captures a frame
-    # Displays the frame, until 'q' is pressed
+    success, img = cap.read()
     cv2.imshow("Camera", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-# releases the capture from the camera and frees up memory
-
 cap.release()
 cv2.destroyAllWindows()
