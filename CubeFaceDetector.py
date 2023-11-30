eec0c020c20b15667f515d188bf7eae3e3a58944
noam bajo <noambaharjosefson@gmail.com>
2023-11-30 23:35:25 +0200

    Takes a screen shot when the cube face is detected, file name changed

--- CubeFaceDetector.py
+++ CubeFaceDetector.py
@@ -50,16 +50,18 @@ while True:
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.GaussianBlur(img, (3, 3), 1)
     img = cv2.bitwise_and(img, img, mask=mask)
-    _, thresh = cv2.threshold(img, 130, 255, cv2.THRESH_BINARY)
+    _, thresh = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     for contour in contours:
         approx = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour, True), True)
         x2, y2, w, h = cv2.boundingRect(contour)
-        if len(approx) == 4 and 0.92 <= float(w)/h <= 1.08 and 30000 > cv2.contourArea(contour) > 20000:
-            x1 = approx.ravel()[0]
-            y1 = approx.ravel()[1]
-            cv2.putText(showingImage, "square", (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
-            cv2.drawContours(showingImage, [approx], 0, (0, 0, 255), 3)
+        x1 = approx.ravel()[0]
+        y1 = approx.ravel()[1]
+        if len(approx) == 4 and 0.92 <= float(w)/h <= 1.08 and 25000 > cv2.contourArea(contour) > 20000:
+            cv2.imwrite('Capture.jpg', showingImage[200:400, 150:350])
+            break
+    cv2.putText(showingImage, "square", (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
+    cv2.drawContours(showingImage, [approx], 0, (0, 0, 255), 3)
     cv2.imshow("Camera", showingImage)
     cv2.imshow("Computation", img)
     cv2.imshow("Threshold", thresh)
