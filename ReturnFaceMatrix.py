4abd72f6be87791027f871aa89d98dba870603c2
noam bajo <noambaharjosefson@gmail.com>
2023-12-24 03:53:31 +0200

    trained the mask values, picture taken on a 'good' camera, hope the values stay the same, regardless of cam and pos

--- ReturnFaceMatrix.py
+++ ReturnFaceMatrix.py
@@ -2,31 +2,17 @@ import cv2
 import numpy as np
 
 img = cv2.imread("Capture.jpg")
-'''
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    MinHue = cv2.getTrackbarPos("MinHue", "Trackbars")
-    MaxHue = cv2.getTrackbarPos("MaxHue", "Trackbars")
-    MinSat = cv2.getTrackbarPos("MinSat", "Trackbars")
-    MaxSat = cv2.getTrackbarPos("MaxSat", "Trackbars")
-    MinVal = cv2.getTrackbarPos("MinVal", "Trackbars")
-    MaxVal = cv2.getTrackbarPos("MaxVal", "Trackbars")
 
-    BLUE_MASK = cv2.inRange(hsv, np.array([108, 0, 115]), np.array([121, 255, 184]))
-    GREEN_MASK = cv2.inRange(hsv, np.array([73, 99, 81]), np.array([92, 255, 255]))
-    ORANGE_MASK = cv2.inRange(hsv, np.array([MinHue, MinSat, MinVal]), np.array([MaxHue, MaxSat, MaxVal]))
-    YELLOW_MASK = cv2.inRange(hsv, np.array([22, 59, 35]), np.array([28, 252, 255]))
-    
-    cv2.namedWindow("Trackbars")
-cv2.resizeWindow("Trackbars", 640, 240)
-cv2.createTrackbar("MinHue", "Trackbars", 0, 179, empty)
-cv2.createTrackbar("MaxHue", "Trackbars", 70, 179, empty)
-cv2.createTrackbar("MinSat", "Trackbars", 110, 255, empty)
-cv2.createTrackbar("MaxSat", "Trackbars", 184, 255, empty)
-cv2.createTrackbar("MinVal", "Trackbars", 135, 255, empty)
-cv2.createTrackbar("MaxVal", "Trackbars", 255, 255, empty)
+BLUE_MASK = cv2.inRange(hsv, np.array([110, 224, 0]), np.array([134, 255, 255]))
+GREEN_MASK = cv2.inRange(hsv, np.array([62, 135, 31]), np.array([103, 255, 69]))
+WHITE_MASK = cv2.inRange(hsv, np.array([102, 22, 143]), np.array([118, 100, 218]))
+YELLOW_MASK = cv2.inRange(hsv, np.array([22, 132, 164]), np.array([30, 246, 207]))
+ORANGE_MASK = cv2.inRange(hsv, np.array([6, 200, 186]), np.array([11, 255, 253]))
+RED_MASK = cv2.inRange(hsv, np.array([0, 176, 143]), np.array([5, 255, 226]))
 
+cv2.imshow("Image", img)
+cv2.imshow("Mask", cv2.bitwise_and(img, img, mask=RED_MASK))
 
-def empty(a):
-    pass
-
-'''
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
